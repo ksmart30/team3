198da878662b5c3160988dddf4d306a7dea12789
pgh9411 <gnsl811@naver.com>
2019-04-03 13:46:19 +0900

    [team3-박기훈] M/H 계획/실적(프로젝트 누계) - AJAX사용해서 프로젝트 코드 받아오기, Layout 수정 적용
    detail:
    
    <-View->
    result_plan_nugae : ajax로 프로젝트 코드 출력하기
    
    
    insapyeonga_cate_list.html : css 추가
    insapyeonga_chart_list.html : css 추가
    insapyeonga_in.html : css 추가
    insapyeonga_result_list.html : css 추가
    insapyeonga_result_search.html : css 추가
    
    register_list.html : css 추가
    register_search.html : css 추가
    yidong_in.html : css 추가
    yidong_list.html : css 추가
    yidong_up.html : css 추가
    
    <-Controller->
    ResultNugaeController.java : ajax로 프로젝트 코드 출력 메서드 경로설정
    <-Service->
    ResultNugaeService.java : ajax로 프로젝트 코드 출력 메서드
    <-Mapper->
    ResultNugaeMapper.xml : ajax로 프로젝트 코드 출력 쿼리
    ResultNugaeMapper.java : ajax로 프로젝트 코드 출력 쿼리

--- ksmart30/src/main/java/com/cafe24/ksmart30/team03/mh/controller/ResultNugaeController.java
+++ ksmart30/src/main/java/com/cafe24/ksmart30/team03/mh/controller/ResultNugaeController.java
@@ -1,5 +1,32 @@
 package com.cafe24.ksmart30.team03.mh.controller;
 
-public class ResultNugaeController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cafe24.ksmart30.team03.mh.service.ResultNugaeService;
+import com.cafe24.ksmart30.team03.mh.vo.ResultList;
+
+@Controller
+
+public class ResultNugaeController {
+	@Autowired private ResultNugaeService resultNugaeService;
+	// M/H 계획/실적(프로젝트 누계) list가져오기
+	
+	@GetMapping("/resultplanlist")
+	public @ResponseBody List<ResultList> resultPlanNugae2() {
+		System.out.println("getPjtcd 메서드 호출");
+		List<ResultList> result = resultNugaeService.getPjtcd();
+		return result;
+	}
+	
+	// 화면 먼저 출력
+	@GetMapping("/manHour/plan/nugae")
+	public String resultPlanNugae() {
+		System.out.println("resultPlanNugae 메서드 호출");
+		return "mh/result/result_plan_nugae";
+	}
 }
--- ksmart30/src/main/java/com/cafe24/ksmart30/team03/mh/service/ResultNugaeService.java
+++ ksmart30/src/main/java/com/cafe24/ksmart30/team03/mh/service/ResultNugaeService.java
@@ -1,5 +1,19 @@
 package com.cafe24.ksmart30.team03.mh.service;
 
-public class ResultNugaeService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cafe24.ksmart30.team03.mh.mapper.ResultNugaeMapper;
+import com.cafe24.ksmart30.team03.mh.vo.ResultList;
+
+@Service
+public class ResultNugaeService {
+@Autowired ResultNugaeMapper resultNugeMapper;
+	//list에 프로젝트코드pjt_cd 뿌려주기
+	public List<ResultList> getPjtcd(){
+		System.out.print("getPjtcd 메서드 호출(service)");
+		return resultNugeMapper.getPjtcd();
+	}
 }
