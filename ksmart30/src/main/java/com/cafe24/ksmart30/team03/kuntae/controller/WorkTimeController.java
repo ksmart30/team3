07c943feb478bdac21116af92e79bc78f213d0ca
dageum3838 <jjdk3838@naver.com>
2019-04-03 17:44:32 +0900

    [team3-정다금] 근태관리, M/H 계획,실적 네이밍 변경
    detail:
    <-Controller->
    WorkTimeController.java : URL주소와 VIEW 리턴값 수정
    BreakApplyController.java : URL주소와 VIEW 리턴값 수정
    KuntaeController.java : URL주소와 VIEW 리턴값 수정
    
    <-View->
    breakapply_list.html -> breakApplyView.html: VIEW 파일명 변경
    breakapply_in.html -> breakApplyWriteView.html: VIEW 파일명 변경
    day_search.html -> daySearchView.html: VIEW 파일명 변경
    okList.html -> okView.html: VIEW 파일명 변경
    worktime_record_single.html -> recordSingleView.html: VIEW 파일명 변경
    worktime_record_list.html -> recordTotalView.html: VIEW 파일명 변경
    kuntae_ok_list.html -> searchView.html: VIEW 파일명 변경
    worktime_in.html -> workTimeView.html: VIEW 파일명 변경

--- ksmart30/src/main/java/com/cafe24/ksmart30/team03/kuntae/controller/WorkTimeController.java
+++ ksmart30/src/main/java/com/cafe24/ksmart30/team03/kuntae/controller/WorkTimeController.java
@@ -14,17 +14,28 @@ public class WorkTimeController {
 	 * workTimeRecordSearch 출, 퇴근 기록부 (검색 처리)
 	 */
 	
-	// 일일 근무 현황
-	@GetMapping("/kuntae/daySearch")
-	public String daySearch() {
-		return "kuntae/day_search";
+	// 일일 근무 현황 (조회)
+	@GetMapping("/kuntae/daySearchView")
+	public String daySearchView() {
+		return "/kuntae/daySearchView";
 	}
 	
+	// 출, 퇴근 기록부 (조회)
+	@GetMapping("/kuntae/recordTotalView")
+	public String recordTotalView() {
+		return "/kuntae/recordTotalView";
+	}
+	
+	// 출, 퇴근 기록부 (개인별)
+	@GetMapping("/kuntae/recordSingleView")
+	public String recordSingleView() {
+		return "/kuntae/recordSingleView";
+	}	
 	
 	// 출, 퇴근 정정 신청
-	@GetMapping("/kuntae/workTimeIn")
-	public String breakApplyInsertForm() {
-		return "kuntae/worktime_in";
+	@GetMapping("/kuntae/workTimeView")
+	public String workTimeView() {
+		return "/kuntae/workTimeView";
 	}
 	
 	// 출, 퇴근 정정 신청 처리
@@ -37,19 +48,5 @@ public class WorkTimeController {
 	@PostMapping("/kuntae/workTimeRecord")
 	public String workTimeRecordSearch() {
 		return "kuntae/worktime_record_search";
-	}
-	
-	// 출, 퇴근 기록부 (List->Total로 변경)
-	@GetMapping("/kuntae/recordTotal")
-	public String workTimeRecordTotal() {
-		return "kuntae/worktime_record_list";
-	}
-	
-	// 출, 퇴근 기록부 (개인별)
-	@GetMapping("/kuntae/recordSingle")
-	public String workTimeRecordSingle() {
-		return "kuntae/worktime_record_single";
-	}
-	
-	
+	}	
 }
