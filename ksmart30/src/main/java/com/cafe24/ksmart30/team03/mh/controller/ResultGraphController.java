ceb2f9c4b31b22b958df61575f8e600aeda2e111
Yoojisu <jungi__@naver.com>
2019-04-03 17:26:05 +0900

    [team3-유지수] 인사기록, M/H 계획,실적 네이밍 변경
    detail:
    <-Controller->
    PersonController.java : URL주소와 VIEW 리턴값 수정
    ResultGraphController.java : URL주소와 VIEW 리턴값 수정
    
    <-View->
    person_in.html -> personInView.html : VIEW 파일명 변경
    person_search.html -> personSearchView.html : VIEW 파일명 변경
    person_listView.html -> personListView.html : VIEW 파일명 변경
    person_up.html -> personUpView.html : VIEW 파일명 변경
    result_graph.html -> resultPlanGraphView.html : VIEW 파일명 변경

--- ksmart30/src/main/java/com/cafe24/ksmart30/team03/mh/controller/ResultGraphController.java
+++ ksmart30/src/main/java/com/cafe24/ksmart30/team03/mh/controller/ResultGraphController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+/*import org.springframework.web.bind.annotation.PostMapping;*/
 import org.springframework.web.bind.annotation.ResponseBody;
 /*import org.springframework.web.bind.annotation.RestController;*/
 
@@ -16,15 +16,15 @@ import com.cafe24.ksmart30.team03.mh.vo.ResultList;
 public class ResultGraphController {
 	@Autowired private ResultGraphService resultGraphService;
 	// M/H 계획/실적(그래프) e
-		@GetMapping("/manHour/plan/graph")
-		public String resultPlanGraph() {
+		@GetMapping("/manHour/manHourPlanGraphView")
+		public String manHourPlanGraphView() {
 			System.out.println("CONTROLLER : M/H계획 실적 화면으로 이동");
-			return "mh/result/result_plan_graph";
+			return "mh/result/resultPlanGraphView";
 		}
 		
-		
-		@GetMapping("/list")
-		public @ResponseBody List<ResultList> resultPlanGraphList() {
+		// M/H 계획/실적(그래프) 리스트 출력 컨트롤러
+		@GetMapping("/manHour/manHourResultGraphList")
+		public @ResponseBody List<ResultList> manHourPlanGraphList() {
 			System.out.println("CONTROLLER : M/H계획 실적 그래프 리스트 출력");	
 			List<ResultList> data = resultGraphService.resultSearch();
 			System.out.println(data);
