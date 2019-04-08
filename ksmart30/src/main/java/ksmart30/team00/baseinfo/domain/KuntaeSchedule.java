b66a5ba099a033ef95ead6bffb2e3888ce6edb97
mushbird <0109bsh@gmail.com>
2019-04-08 19:31:37 +0900

    [team3-백승한] 근태월력표 리스트 조회 Domain, Mapper 작성
    datail :
    근태월력표 리스트 조회에 필요한
    Domain, Mapper 작성
    
    <-Mapper->
    - KuntaeScheduleMapper : 근태월력표 리스트 조회 인터페이스 선언
    - KuntaeScheduleMapper(xml) : 근태월력표 리스트 조회 쿼리 작성(왼쪽 리스트)

--- ksmart30/src/main/java/ksmart30/team00/baseinfo/domain/KuntaeSchedule.java
+++ ksmart30/src/main/java/ksmart30/team00/baseinfo/domain/KuntaeSchedule.java
@@ -1,5 +1,44 @@
 package ksmart30.team00.baseinfo.domain;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class KuntaeSchedule {
 
+    private String M_NUM;
+    private String M_PART;
+    private String M_NAME;
+    private String WK;
+    private String TODAY;
+
+    public String getM_NUM() {
+        return M_NUM;
+    }
+    public void setM_NUM(String m_NUM) {
+        M_NUM = m_NUM;
+    }
+    public String getM_PART() {
+        return M_PART;
+    }
+    public void setM_PART(String m_PART) {
+        M_PART = m_PART;
+    }
+    public String getM_NAME() {
+        return M_NAME;
+    }
+    public void setM_NAME(String m_NAME) {
+        M_NAME = m_NAME;
+    }
+    public String getWK() {
+        return WK;
+    }
+    public void setWK(String wK) {
+        WK = wK;
+    }
+    public String getTODAY() {
+        return TODAY;
+    }
+    public void setTODAY(String tODAY) {
+        TODAY = tODAY;
+    } 
 }
--- ksmart30/src/main/java/ksmart30/team00/baseinfo/mapper/KuntaeScheduleMapper.java
+++ ksmart30/src/main/java/ksmart30/team00/baseinfo/mapper/KuntaeScheduleMapper.java
@@ -1,5 +1,14 @@
 package ksmart30.team00.baseinfo.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import ksmart30.team00.baseinfo.domain.KuntaeSchedule;
+
+@Mapper
 public interface KuntaeScheduleMapper {
 
+    // 10.3.5.1 근태월력표 조회(왼쪽)
+    List<KuntaeSchedule> getKuntaeScheduleLeftList();
 }
