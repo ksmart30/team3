fb7002c602c319962a4aed554ad7f4573f1f2e7e
mushbird <0109bsh@gmail.com>
2019-04-08 17:25:44 +0900

    [team3-백승한] 학교코드 등록, 전공코드 등록 리스트 완료
    detail :
    학교코드 등록 리스트 화면
    전공코드 등록 리스트 화면 출력까지
    리스트 클릭시 수정폼에 자동으로 입력
    
    <-Controller->
    - MajorController : 전공코드 조회 매서드 완료
    - SchoolController :  학교코드 조회 매서드 완료
    
    <-Service->
    - MajorService : 전공코드 조회 매서드 완료
    - SchoolService : 학교코드 조회 매서드 완료
    
    <-domain->
    - Major : 전공코드 VO 작성
    - School : 학교코드 VO 작성
    
    <-View->
    - majorView : 전공코드 등록 화면 완료
    - schoolView : 학교코드 등록 화면 완료
    - departmentView : 부서코드 등록 리스트 클릭시 색상 변경, 잘못 작성된 font태그 삭제

--- ksmart30/src/main/java/ksmart30/team00/baseinfo/domain/Major.java
+++ ksmart30/src/main/java/ksmart30/team00/baseinfo/domain/Major.java
@@ -1,5 +1,27 @@
 package ksmart30.team00.baseinfo.domain;
 
-public class Major {
+import org.springframework.stereotype.Component;
 
+@Component
+public class Major {
+    private String MAJOR_CD;
+    private String MAJOR_NM;
+    
+	public String getMAJOR_CD() {
+		return MAJOR_CD;
+	}
+	public void setMAJOR_CD(String mAJOR_CD) {
+		MAJOR_CD = mAJOR_CD;
+	}
+	public String getMAJOR_NM() {
+		return MAJOR_NM;
+	}
+	public void setMAJOR_NM(String mAJOR_NM) {
+		MAJOR_NM = mAJOR_NM;
+	}
+	
+	@Override
+	public String toString() {
+		return "Major [MAJOR_CD=" + MAJOR_CD + ", MAJOR_NM=" + MAJOR_NM + "]";
+	}
 }
--- ksmart30/src/main/java/ksmart30/team00/baseinfo/domain/School.java
+++ ksmart30/src/main/java/ksmart30/team00/baseinfo/domain/School.java
@@ -1,5 +1,34 @@
 package ksmart30.team00.baseinfo.domain;
 
-public class School {
+import org.springframework.stereotype.Component;
 
+@Component
+public class School {
+    private String SCHOOL_GB;
+    private String SCHOOL_CD;
+    private String SCHOOL_NM;
+    
+	public String getSCHOOL_GB() {
+		return SCHOOL_GB;
+	}
+	public void setSCHOOL_GB(String sCHOOL_GB) {
+		SCHOOL_GB = sCHOOL_GB;
+	}
+	public String getSCHOOL_CD() {
+		return SCHOOL_CD;
+	}
+	public void setSCHOOL_CD(String sCHOOL_CD) {
+		SCHOOL_CD = sCHOOL_CD;
+	}
+	public String getSCHOOL_NM() {
+		return SCHOOL_NM;
+	}
+	public void setSCHOOL_NM(String sCHOOL_NM) {
+		SCHOOL_NM = sCHOOL_NM;
+	}
+	
+	@Override
+	public String toString() {
+		return "School [SCHOOL_GB=" + SCHOOL_GB + ", SCHOOL_CD=" + SCHOOL_CD + ", SCHOOL_NM=" + SCHOOL_NM + "]";
+	}
 }
