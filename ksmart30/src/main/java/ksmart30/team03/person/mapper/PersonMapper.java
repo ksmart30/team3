1598e4b2785c086c07445c901ba53ffb0e52d484
Yoojisu <jungi__@naver.com>
2019-04-09 17:27:40 +0900

    [team3-유지수] 인사기록 직원검색 list 출력
    detail:
    
    <-Controller->
    PersonController.java : 전체 직원검색 컨트롤러, 한 행 클릭시 해당하는 직원의 디테일 정보를 가져오는 컨트롤러
    작성
    
    <-Mapper->
    PersonMapper.java : 전체 직원검색, 해당 직원의 디테일 출력하는 쿼리,메서드 작성
    
    <-Service->
    PersonService.java : 전체 직원검색 서비스, 한 행 클릭시 해당하는 직원의 디테일 정보를 가져오는서비스 작성
    
    <-View->
    personSearchView.html : ajax사용하여 전체직원 list출력 완료,해당직원의  detail 정보 출력 진행중
    
    personSearchView.html : 그래프 글짜 크기 수정

--- ksmart30/src/main/java/ksmart30/team03/person/mapper/PersonMapper.java
+++ ksmart30/src/main/java/ksmart30/team03/person/mapper/PersonMapper.java
@@ -1,5 +1,14 @@
 package ksmart30.team03.person.mapper;
 
-public interface PersonMapper {
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import ksmart30.team03.person.domain.Person;
 
+@Mapper
+public interface PersonMapper {
+	//6.1.2 직원 list출력(전체)
+	List<Person> personInsaEmployeeSearchView();
+	//6.1.2 직원 디테일 출력
+	List<Person> personInsaEmployeeDetailView(String EMP_NO);
+	
 }
--- ksmart30/src/main/java/ksmart30/team03/person/service/PersonService.java
+++ ksmart30/src/main/java/ksmart30/team03/person/service/PersonService.java
@@ -1,5 +1,25 @@
 package ksmart30.team03.person.service;
 
-public class PersonService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ksmart30.team03.person.domain.Person;
+import ksmart30.team03.person.mapper.PersonMapper;
+
+@Service
+public class PersonService {
+	
+	@Autowired PersonMapper personMapper;	
+	//6.1.2 직원 list출력(전체)
+	public List<Person> personInsaEmployeeSearchView(){
+		System.out.println("SERVICE : 직원검색 리스트 출력");
+		return personMapper.personInsaEmployeeSearchView();		
+	}
+	//6.1.2 직원 디테일 출력
+	public List<Person> personInsaEmployeeDetailView(String EMP_NO){
+		System.out.println("SERVICE : 직원검색 리스트 출력");
+		return personMapper.personInsaEmployeeDetailView(EMP_NO);		
+	}
 }
