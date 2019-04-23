3e1478f3c08db5ea19dcd5fe07e4231ad742aa76
mushbird <0109bsh@gmail.com>
2019-04-23 16:07:01 +0900

    [team3-백승한] 급여대장 조회 및 출력완료
    detail :
    급여 대장 검색과 조회 완료
    아직 콤마 변환은 미완료
    
    <-Controller->
    PayController :  급여 대장 조회 및 검색 (월간, 연간)
    
    <-Domain->
    PayMonth : 월간 검색시 필요한 변수 추가
    PayYear : 연간 검색시 필요한 변수 추가
    
    <-Mapper->
    PayMapper : 급여대장 검색 메서드 선언및 쿼리문 작성
    
    <-Service->
    PayService : 급여 대장 검색 처리 메서드 추가
    
    <-View->
    personPayRegisterView : 월간 급여 수정과 연간 급여화면 추가 , 기능 완료

--- ksmart30/src/main/java/ksmart30/team03/person/mapper/PayMapper.java
+++ ksmart30/src/main/java/ksmart30/team03/person/mapper/PayMapper.java
@@ -5,11 +5,19 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import ksmart30.team03.person.domain.PayGiveTake;
+import ksmart30.team03.person.domain.PayMonth;
+import ksmart30.team03.person.domain.PayYear;
 
 @Mapper
 public interface PayMapper {
 
     // 1. 급여 입력 및 계산조회
     List<PayGiveTake> getPersonPayCalList(PayGiveTake payGiveTake);
+
+    // 2. 급여 대장 검색 (월간)
+    List<PayMonth> getPersionPayMonthList(PayMonth payMonth);
+
+    // 3. 급여 대장 검색 (연간)
+    List<PayYear> getPersionPayYearList(PayYear payYear);
 
 }
--- ksmart30/src/main/java/ksmart30/team03/person/service/PayService.java
+++ ksmart30/src/main/java/ksmart30/team03/person/service/PayService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ksmart30.team03.person.domain.PayGiveTake;
+import ksmart30.team03.person.domain.PayMonth;
+import ksmart30.team03.person.domain.PayYear;
 import ksmart30.team03.person.mapper.PayMapper;
 
 @Service
@@ -21,4 +23,23 @@ public class PayService {
         return giveTakeList;
     }
 
+    // 6.4.1.3 급여 대장 검색 (월간)
+    public List<PayMonth> getPersionPayMonthList(PayMonth payMonth) {
+        System.out.println("(S) 6.4.1.3 급여 대장 검색 (월간) getPersionPayMonthList()");
+        // 1. Mapper 호출 (결과값 : 사원 월간 급여정보)
+        List<PayMonth> searchPayMonth = payMapper.getPersionPayMonthList(payMonth);
+        System.out.println("(M) 6.4.1.3 급여 대장 검색 (월간) 쿼리 성공");
+        // 2. 결과값 리턴 
+        return searchPayMonth;
+    }
+
+    // 6.4.1.4 급여 대장 검색 (연간)
+    public List<PayYear> getPersionPayYearList(PayYear payYear) {
+        System.out.println("(S) 6.4.1.4 급여 대장 검색 (연간) getPersionPayYearList()");
+        // 1. Mapper 호출 (결과값 : 사원 연간 급여정보)
+        List<PayYear> searchPayYear = payMapper.getPersionPayYearList(payYear);
+        System.out.println("(M) 6.4.1.4 급여 대장 검색 (연간) 쿼리 성공");
+        // 2. 결과값 리턴 
+        return searchPayYear;
+    }
 }
