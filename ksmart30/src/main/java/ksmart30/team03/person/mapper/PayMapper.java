f6c41653a4444c27f0edc19740c14faf0111cb23
mushbird <0109bsh@gmail.com>
2019-04-25 14:28:38 +0900

    [team3-백승한] 부서코드 등록 검색, 유효성검사(검색결과) 및 전체주석 수정
    detail :
    전체 주석 수정 (정해진 형식으로)
    부서코드 검색 기능 추가
    급여 대장 검색시 결과값이 없다면 없다고 출력
    User-ID 수정폼 CSS 수정
    
    <-Controller->
    DepartmentController : 검색 메서드 추가
    UserIdController : 주석 수정
    
    <-Mapper->
    DepartmentMapper : 조회를 검색 조회로 수정, 쿼리문 추가
    GapguenseMapper : 주석 수정
    InsureMapper : 주석 수정
    InsureSanMapper : 주석 수정
    PayMapper : 주석 수정
    
    <-Service->
    DepartmentService : 조회 메서드 수정, 검색처리 메서드 추가
    
    <-View->
    departmentView : 부서코드 검색 기능 추가
    personPayCalWriteView : CSS 수정
    personPayRegisterView : 검색 결과 없다면  처리 추가
    userIdView : CSS 수정

--- ksmart30/src/main/java/ksmart30/team03/person/mapper/PayMapper.java
+++ ksmart30/src/main/java/ksmart30/team03/person/mapper/PayMapper.java
@@ -12,16 +12,16 @@ import ksmart30.team03.person.domain.PayYear;
 @Mapper
 public interface PayMapper {
 
-    // 1. 급여 계산 (지급, 공제항목)
+    // 6.4.1.2 급여 계산 (지급, 공제항목)
     List<PayGiveTake> getPersonPayCalList(PayGiveTake payGiveTake);
 
-    // 2. 급여 계산 (기간적용항목 / 지급 및 공제)
+    // 6.4.1.3 급여 계산 (기간적용항목 / 지급 및 공제)
     List<PayPeriod> getPersonPayCalPeriodList(PayPeriod payPeriod);
 
-    // 3. 급여 대장 검색 (월간)
+    // 6.4.1.5 급여 대장 검색 (월간)
     List<PayMonth> getPersionPayMonthList(PayMonth payMonth);
 
-    // 4. 급여 대장 검색 (연간)
+    // 6.4.1.6 급여 대장 검색 (연간)
     List<PayYear> getPersionPayYearList(PayYear payYear);
 
 }
