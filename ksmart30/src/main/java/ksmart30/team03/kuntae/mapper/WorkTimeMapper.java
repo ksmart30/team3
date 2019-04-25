c8fae99fd471bca66ffdeb24896d1f806543dcf7
dageum3838 <jjdk3838@naver.com>
2019-04-25 16:42:16 +0900

    [team3-정다금] 근태관리 - 휴가 신청 Detail 출력
    detail:
    <-Issue->
    휴가 신청 리스트 한 행 클릭 시 사원정보에 따른 휴가 신청 리스트 출력
    
    <-Controller->
    BreakApplyController.java : 휴가 신청 Detail 메서드 추가.
    
    <-Mapper->
    BreakApplyMapper.xml : 휴가 신청 Detail 쿼리문 입력.
    BreakApplyMapper.java : 휴가 신청 Detail 메서드 입력.
    
    <-Service->
    BreakApplyService.java : 휴가 신청 Detail 서비스 메서드 입력.
    
    <-View->
    breakApplyView.html : Detail Modal추가, 휴가 신청서 버튼 이동.

--- ksmart30/src/main/java/ksmart30/team03/kuntae/mapper/WorkTimeMapper.java
+++ ksmart30/src/main/java/ksmart30/team03/kuntae/mapper/WorkTimeMapper.java
@@ -9,37 +9,35 @@ import ksmart30.team03.kuntae.domain.WorkTimeSingleList;
 
 @Mapper
 public interface WorkTimeMapper {
-	// 5.3 출퇴근기록부(개인별) List 출력
-	/* List<WorkTimeSingleList> getRecordSingleList(); */
 	
-	// 출 퇴근 기록부(개인별) 사원번호 검색
+	// 4.03 출 퇴근 기록부(개인별) 사원번호 검색
 	List<WorkTimeSingleList> getRecordSingleNoSearch(String EMP_NO);
 	
-	// 출 퇴근 기록부(개인별) 이름 검색
+	// 4.03 출 퇴근 기록부(개인별) 이름 검색
 	List<WorkTimeSingleList> getRecordSingleNameSearch(String KOR_NM);
 	
-	// 출 퇴근 기록부 (Single, Total) 날짜 검색
+	// 4.02,03 출 퇴근 기록부 (Single, Total) 날짜 검색
 	List<WorkTimeSingleList> getRecordSingleDateSearch(String WORK_DT, String END_DT);
 	
-	// 출 퇴근 기록부(Total) 사원번호 검색
+	// 4.02 출 퇴근 기록부(Total) 사원번호 검색
 	List<WorkTimeSingleList> getRecordTotalNoSearch(String EMP_NO);
 		
-	// 출 퇴근 기록부(Total) 당일 출 퇴근 명단 출력
+	// 4.02 출 퇴근 기록부(Total) 당일 출 퇴근 명단 출력
 	List<WorkTimeSingleList> getRecordTotalList();
 	
-	// 출 퇴근 기록부(Total) 전체 검색 
+	// 4.02 출 퇴근 기록부(Total) 전체 검색 
  	List<WorkTimeSingleList> getRecordTotalSearch(String WORK_DT, String END_DT, String EMP_NO);
  	
  	// 전체 검색(검색 조건들을 vo로 받음)
  	List<Map<String, Object>> getSingleListDomain(WorkTimeSingleList vo);
 	
- 	// 출퇴근 정정신청 List가져오기
+ 	// 4.04 출퇴근 정정신청 List가져오기
  	List<WorkTime> getWorkTimeView();
  	
- 	// 출퇴근 정정신청  List 이름 검색
+ 	// 4.04 출퇴근 정정신청  List 이름 검색
  	List<WorkTimeSingleList> getWorkTimeSearchView(String KOR_NM, String WORK_DT, String WORK_DT1);
  	
- 	// 출퇴근 정정 신청 한 행의 지각 횟수와 지각 누계 정보 가져오기
+ 	// 4.04 출퇴근 정정 신청 한 행의 지각 횟수와 지각 누계 정보 가져오기
  	List<WorkTime> getWorkTimeJigakSearch(String EMP_NO);
  	
  	
