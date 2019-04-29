f567bfc74865fcf9970e75beef306351db474775
mushbird <0109bsh@gmail.com>
2019-04-29 11:52:37 +0900

    [team3-백승한] body 메뉴 추가, User-ID 수정
    detail :
    프로젝트 종료 화면 추가
    Body 메뉴 추가,
    로그인시 출근, 퇴근 버튼 보이도록 추가
    User-ID 처리 과정 수정
    
    <-Controller->
    UserIdController :  User-ID 등록처리 추가
    ProjectEndController : 프로젝트 종료 화면이동 메서드 선언
    
    <-Domain->
    UserId : VO추가
    
    <-View->
    body : 프로젝트종료 메뉴추가, 출퇴근 버튼 추가
    projectEndView : 프로젝트 종료화면 추가
    userIdView : 상단 토글메뉴 수정, 검색조건 삭제

--- ksmart30/src/main/java/ksmart30/team00/system/controller/UserIdController.java
+++ ksmart30/src/main/java/ksmart30/team00/system/controller/UserIdController.java
@@ -9,6 +9,16 @@ public class UserIdController {
 	// 11.1.1 User-ID 화면
 	@GetMapping("/system/userIdSearchView")
 	public String userIdSearchView() {
+		System.out.println("(C) 11.1.1 User-ID 화면 userIdSearchView()");
 		return "system/userIdView";
+	}
+
+	// 11.1.2 User-ID 등록처리
+	@GetMapping("/system/userIdWriteProcess")
+	public int UserIdWriteProcess() {
+		System.out.println("(C) 11.1.2 User-ID 등록처리 userIdSearchView()");
+		
+		int result = 0;
+		return result;
 	}
 }
